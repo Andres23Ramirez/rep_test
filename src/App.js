10d58bc78c352a23d252f93e631f43da39b73520
Rodrigo23Ramirez <rodrigo23ramirez@gmail.com>
2020-12-17 19:16:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React como 
         </a>
       </header>
     </div>
